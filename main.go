32733fcd5f56234a2812f2bc974f86309f89c4dd
Matt Freeman <matt@nowprovision.com>
2015-10-11 14:46:26 +0700

    inital commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "net"
+import "time"
+import "net/http"
+import "fmt"
+
+import "nowprovision/webhookproxy"
+
+func main() {
+
+	fmt.Println("Starting webhookproxy server on port 8080")
+
+	config := webhookproxy.Config{}
+	config.TryLaterStatusCode = 503
+	config.BackQueueSize = 100
+	config.MaxWaitSeconds = 10 * time.Second
+	config.UseLongPoll = true
+	config.MaxPayloadSize = 5000000
+
+	_, localNetwork, _ := net.ParseCIDR("127.0.0.1/24")
+	config.WebhookWhiteList = []*net.IPNet{localNetwork}
+	config.PollReplyWhiteList = []*net.IPNet{localNetwork}
+
+	webhookproxy.RegisterHandlers(config, http.DefaultServeMux)
+	http.ListenAndServe(":8080", nil)
+}
